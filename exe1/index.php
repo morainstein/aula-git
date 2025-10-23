90c5fae74f3d7651c4833deaf6324a0d72b90b15
morainstein <nichroberto@gmail.com>
2025-10-22 21:24:53 -0300

    exec1.1

--- exe1/index.php
+++ exe1/index.php
@@ -1,7 +1,6 @@
 <?php
-require_once 'Livro.php';
+// require_once 'Livro.php';
 
 $livro = new Livro('1984', 'George Orwell', 328);
 echo "Título: " . $livro->getTitulo() . PHP_EOL;
-echo "Autor: " . $livro->getAutor() . PHP_EOL;
 echo "Número de Páginas: " . $livro->getNumeroDePaginas() . PHP_EOL;
